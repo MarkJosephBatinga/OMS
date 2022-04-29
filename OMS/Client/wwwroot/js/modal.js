50227b03ed4d5d83d1bbdba89558f5ecf9f3efd0
Mark Joseph Batinga <batingamarkjoseph@gmail.com>
2022-04-29 21:53:22 +0800

    Add Navigation Js

--- OMS/Client/wwwroot/js/modal.js
+++ OMS/Client/wwwroot/js/modal.js
@@ -54,4 +54,29 @@ function loadJs() {
     })
 }
 
+function loadNav() {
+    const body = document.querySelector('body'),
+        sidebar = body.querySelector('.sidebar'),
+        toggle = body.querySelector('.toggle'),
+        searchBar = body.querySelector('.search-bar'),
+        modeSwitch = body.querySelector('.toggle-switch'),
+        modeText = body.querySelector('.mode-text');
+
+
+    toggle.addEventListener('click', () => {
+        sidebar.classList.toggle('close');
+    });
+
+    modeSwitch.addEventListener('click', () => {
+        body.classList.toggle('dark');
+
+        if (body.classList.contains('dark')) {
+            modeText.innerText = 'Light Mode'
+        }
+        else {
+            modeText.innerText = 'Dark Mode'
+        }
+    });
+}
+
 
